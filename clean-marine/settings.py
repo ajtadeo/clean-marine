67b0dd7777255541cebcf6021e88bd7a3a254899
Alyssa Tadeo <alyssa.tadeo@paramount.com>
2023-08-10 10:35:15 -0400

    fix surfrider scraper and add it as an invoke command

--- clean-marine/settings.py
+++ clean-marine/settings.py
@@ -133,8 +133,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'components', 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
---
+++ tasks.py
@@ -0,0 +1,129 @@
+# invoke commands
+from invoke import task, run
+import os
+import django
+# from xml.etree import ElementTree
+# from os import listdir
+# from os.path import isfile, join
+
+# this setup needs to be at the top in order to execute the webscraper the app's context
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clean-marine.settings")
+django.setup()
+
+# web scraping
+# from bs4 import BeautifulSoup
+# import json
+# from datetime import datetime
+import urllib.parse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webscraper.models import Events
+
+# @task
+# def commit(context, name='base'):
+#   """
+#   Shortcut for committing your changes
+
+#   Args:
+#       context: required for all invoke tasks
+#       name: name of the developer (defaults to base)
+
+#   To do:
+#       - check that coverage is up to date
+
+#   Example:    invoke commit --name=amy
+
+SURFRIDER_URL = 'https://volunteer.surfrider.org'
+
+
+@task
+def scrape(context, env='cm_base'):
+    """ Run webscrapers for all websites """
+    scrape_surfrider(context, env)
+
+
+def scrape_surfrider(context, env):
+    """ Run webscraper for Surfrider website """
+    try:
+        print('Starting Surfrider web scraper...')
+        # set up options
+        options = Options()
+        options.add_argument('--headless=new')
+        options.add_argument("--disable-gpu")
+        options.add_argument('--blink-settings=imagesEnabled=false')
+
+        # set up driver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        driver.get(SURFRIDER_URL)
+
+        # wait for page to load, then scrape all available events
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "article.opportunityCard"))
+        )
+        events = driver.find_elements(By.CSS_SELECTOR, "article.opportunityCard")
+        print(str(len(events)) + " events found.")
+        events_successful = 0
+        for event in events:
+            events_successful += scrape_surfrider_event(event)
+        print(str(events_successful) + " events scraped successfully.")
+
+    except Exception as err:
+        print('[ERROR] The scraping job failed. See exception:')
+        print(err)
+    print("Done.")
+
+
+def scrape_surfrider_event(event):
+    """ Helper function for Surfrider web scraping and saving to DB """
+
+    # scrape information from the event card
+    try:
+        link = event.find_element(By.TAG_NAME, "a").get_attribute("href")
+        eventname = event.find_element(By.TAG_NAME, "h2").text
+        organization = event.find_element(By.TAG_NAME, "h3").text
+
+        datetime_location = event.find_elements(By.TAG_NAME, "li")
+        datetime = datetime_location[0].text
+        if (len(datetime_location) == 2):
+            location = datetime_location[1].text
+        else:
+            location = ""
+
+        result = {
+            "placeid": urllib.parse.quote_plus(location),
+            "eventname": eventname,
+            "organization": organization,
+            "link": link,
+            "date_and_time": datetime,
+            "location": location,
+        }
+
+        # save to DB
+        # save(result)
+        return 1
+    
+    except Exception as err:
+        print("[ERROR] Scraping Surfrider event card failed.")
+        print(err)
+        return 0
+
+
+def save(event):
+    """ Load a JSON format event into the DB """
+    try:
+        Events.objects.create(
+            placeid=event['placeid'],
+            eventname=event['eventname'],
+            organization=event['organization'],
+            link=event['link'],
+            date_and_time=event['date_and_time'],
+            location=event['location'],
+        )
+    except Exception as e:
+        print('[ERROR] Saving to DB failed. See exception:')
+        print(e)
